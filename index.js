fc7971dcecc0d2978cfcb47bc3f7388f6a22f2c3
Сергей Вилков <vilkov@reg.ru>
2018-12-01 14:13:38 +0300

    Init

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(
+    '/javascripts',
+    express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+)
+
+app.get('/', (req, res) => res.render('index', {
+        url: 'index',
+        data: 'test'
+    }
+))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
